4f55aacaf3fc853fffb93270ed38461851c53c1d
aartwwws12339t <taniasidelnyk63@gmail.com>
2021-11-30 13:18:10 +0200

    commit

--- main.js
+++ main.js
@@ -1,61 +1,75 @@
-//разбитие на слова
-console.log("1:")
-function spliit(str,ch) {
+console.log("Реалізувати розбиття стрічки на слова у масив:")
+function spliit(str,ch) 
+{
     return str.split(ch);
 }
-console.log(spliit("Hello world + stuff",' '));
-console.log("2:")
-//все перестановки
-function allswaps(arr,current_word,word_length,results){
-    for(let i = 0;i<arr.length;i++){
+console.log(spliit("How are you doing",' '));
+
+console.log();
+console.log("Усі перестановки слова:")
+function allswaps(arr,current_word,word_length,results)
+{
+    for(let i = 0;i<arr.length;i++)
+    {
         let word=current_word+arr[i];
 
-        if(word.length<word_length){
+        if(word.length<word_length)
+        {
 
             let arr2=[]
-            for(let j=0;j<arr.length;j++){
+            for(let j=0;j<arr.length;j++)
+            {
                 if(j!=i){
                     arr2.push(arr[j])
                 }
 
             }
             allswaps(arr2, word, word_length, results);
-        }else{
+        }else
+        {
             results.push(word);
 
         }
 
     }
 }
-function permutations(str){
+function permutations(str)
+{
     let array = spliit(str,'');
     let results=[];
     allswaps(array,"",array.length,results);
     console.log(results);
 }
-permutations("abcd");
-//нахождение факториала
-console.log("3:")
-function fctrl(n){
-    let total=n;
-    for (let i = 1; i < n; i++){
-        total = total * (n - i);
+permutations("cat");
+
+console.log();
+console.log("Факторіал числа:")
+function factorial(n)
+{
+    let result=n;
+    for (let i = 1; i < n; i++)
+    {
+        result = result * (n - i);
     }
-    return total;
+    return result;
 }
-console.log(fctrl(10))
-//нахождение самого большого количества вхождения
-console.log("4:")
-function entrrFinder(r) {
+console.log(factorial(9));
+
+console.log();
+console.log("Елемент із найбільшою частотою повторень")
+function entrSmth(r) 
+{
     var elist = [];
     var clist = [];
     for (i = 0;i<r.length;i++)
     {
-        if(!elist.includes(r[i])){
+        if(!elist.includes(r[i]))
+        {
             elist.push(r[i]);
             clist.push(0);
             let j = i;
-            while(r.indexOf(r[i],j)!=-1) {
+            while(r.indexOf(r[i],j)!=-1) 
+            {
                 clist[elist.length - 1]++;
                 j = r.indexOf(r[i], j) + 1;
             }
@@ -63,31 +77,41 @@ function entrrFinder(r) {
     }
     console.log(elist,clist);
 }
-entrrFinder("rarggrarargg");
-//нахождение количества дней в месяце
-console.log("5:");
-function mtd(m,y){
-    if(m==2&&y%4&&y>=1582) {
+entrSmth("ttvlvtttve");
+
+console.log();
+console.log("Кількість днів для заданого місяця та року:");
+function days(m,y)
+{
+    if(m==2&&y%4&&y>=1582) 
+    {
         return 29;
     }
-    if(m==2){
+    if(m==2)
+    {
         return 28;
     }
-    if(m%2){
+    if(m%2)
+    {
         return 30;
-    }else{
+    }else
+    {
         return 31;
     }
 }
-console.log(mtd(10,2021));
-//fast sorting
-console.log("6:");
-let arr=[5,4,8,1,4,2,9,7];
-function partition(arr, start, end){
+console.log(days(11,2021));
+
+console.log();
+console.log("Швидке сортування масиву");
+let arr=[10,2,14,1,6,8,5,7];
+function partition(arr, start, end)
+{
     const pivotValue = arr[end];
     let pivotIndex = start;
-    for (let i = start; i < end; i++) {
-        if (arr[i] < pivotValue) {
+    for (let i = start; i < end; i++)
+    {
+        if (arr[i] < pivotValue) 
+        {
             [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
             pivotIndex++;
         }
@@ -95,8 +119,10 @@ function partition(arr, start, end){
     [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]]
     return pivotIndex;
 }
-function quickSort(arr, start, end) {
-    if (start >= end) {
+function quickSort(arr, start, end) 
+{
+    if (start >= end) 
+    {
         return;
     }
     let index = partition(arr, start, end);
--- passenger.js
+++ passenger.js
@@ -1,8 +1,11 @@
 let pas_amount=0;
 let tickets_amount=0;
 let trains_amount=0;
-class ticket {
-    constructor(start,destination,date,train){
+
+class ticket 
+{
+    constructor(start,destination,date,train)
+    {
         this.start = start;
         this.destination = destination;
         this.date=date;
@@ -10,16 +13,21 @@ class ticket {
         this.number  = tickets_amount;
         this.train = train;
     }
-    Getamount(){
+    Getamount()
+    {
         return tickets_amount;
     }
-    Changetrain(tr){
+    Changetrain(tr)
+    {
         this.train = tr
         return 0
     }
 }
-class sold_ticket{
-    constructor(ticket,pas_num){
+
+class sold_ticket
+{
+    constructor(ticket,pas_num)
+    {
         this.start = ticket.start;
         this.destination = ticket.destination;
         this.date=ticket.date;
@@ -28,79 +36,114 @@ class sold_ticket{
         this.train = ticket.train;
     }
 }
-class passenger{
-    constructor(name, surname){
+class passenger
+{
+    constructor(name, surname)
+    {
         this.name = name;
         this.surname = surname;
         pas_amount++;
         this.number=pas_amount;
         this.tickets=new Array();
     }
-    AddTicket(ticket){
+    AddTicket(ticket)
+    {
         let tick=new sold_ticket(ticket,this.number);
         this.tickets.push(tick);
         return tick
     }
-    RemoveTicket(n){
+    RemoveTicket(n)
+    {
         this.tickets.splice(n,1)
         return 0
     }
-    Getamount(){
+    Getamount()
+    {
         return pas_amount;
     }
 }
-class train{
-    constructor(){
+class train
+{
+    constructor()
+    {
         trains_amount++;
         this.number = trains_amount;
         this.stops = new Array();
         this.bought = 0
     }
-    AddStop(stopsarr) {
-        for (let s in stopsarr) {
+    AddStop(stopsarr)
+     {
+        for (let s in stopsarr)
+         {
             this.stops.push(stopsarr[s]);
         }
     }
-    Getamount(){
+    Getamount()
+    {
         return trains_amount;
     }
 }
-function Destruct(arr,ind){
+function Destruct(arr,ind) // видаляє ел з масииву
+{
     delete arr.splice(ind,1);
 }
-function Findby(arr,id){
-    for(let i = 0;i<arr.length;i++){
-        if(arr[i].number==id){
+function Findby(arr,id) //для всіх класів знаходить ел по ід
+{
+    for(let i = 0;i<arr.length;i++)
+    {
+        if(arr[i].number==id)
+        {
             id = i
         }
     }
-    if(typeof(arr[id])=="undefined") {
-        console.log("такого обьекта нет")
+    if(typeof(arr[id])=="undefined") 
+    {
+        console.log("такого об'єкта немає")
     }
     return id;
 }
+
 let passengers = []
 let tickets = []
 let trains = []
 let sold = []
-passengers.push(new passenger("a","b"))
-passengers.push(new passenger("d","c"))
-passengers.push(new passenger("j","j"))
+
+passengers.push(new passenger("Viktor","Lavretskyi"))
+passengers.push(new passenger("Tania","Sidelnyk"))
+passengers.push(new passenger("Mariia","Toronii"))
+console.log(" ")
+console.log("Виводить всіх пасажирів")
 console.log(passengers)
+
+console.log(" ")
+console.log("видаляє 2 людину")
 Destruct(passengers,1)
 console.log(passengers)
-Findby(passengers,2)
-passengers[Findby(passengers,3)].surname="g"
+
+console.log(" ")
+console.log("шукає по id")
+Findby(passengers,3)
+console.log("Міняє прізвище 3")
+passengers[Findby(passengers,3)].surname="T"
 console.log(passengers)
+
+//додаються поїзди
 trains.push(new train())
 trains.push(new train())
+
 trains[0].AddStop(["1","2","3","4","5"])
+
+//ств нові білети
 tickets.push(new ticket("1","5","10.10",1))
 tickets.push(new ticket("2","4","10.11",2))
+
+console.log(" ")
+console.log("всі поїзди і міняєм поїзд на одному тікеті")
 tickets[Findby(tickets,2)].Changetrain(1)
 console.log(trains)
 console.log(tickets)
-console.log("покупка билета")
+
+
 function buytick(ticknum,pasnum)
 {
     passengers[Findby(passengers,pasnum)].AddTicket(tickets[Findby(tickets,ticknum)])
@@ -120,33 +163,47 @@ function stats(){
     let maxst=-1;
     let mintr=-1;
     let minst=100;
-    for(j in trains){
-        for(i in sold){
-            if(sold[i].train==trains[j].number){
+    for(j in trains)
+    {
+        for(i in sold)
+        {
+            if(sold[i].train==trains[j].number)
+            {
                 trains[j].bought++
             }
 
         }
 
-        if(trains[j].bought>maxst){
+        if(trains[j].bought>maxst)
+        {
             maxst=trains[j].bought
             maxtr=j
 
         }
-        if(trains[j].bought<minst){
+        if(trains[j].bought<minst)
+        {
             minst=trains[j].bought
             mintr=j
 
         }
     }
+    
     console.log("Highest profit train:")
     console.log(trains[maxtr])
     console.log("lowest profit train:")
     console.log(trains[mintr])
+    
 }
+console.log(" ")
+console.log("Покупка білета")
 buytick(1,3)
 buytick(2,3)
+
+console.log(" ")
+console.log("скасування для 3 білета")
 Canceltick(1,3)
+console.log("продані")
 console.log(sold)
+console.log(" ")
 stats()
 
